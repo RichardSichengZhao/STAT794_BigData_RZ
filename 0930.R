95f689919b272fddd7c6425b1a5cdd4d34d2ec82
Richard Zhao <richardsichengzhao@gmail.com>
2025-09-30 19:12:16 -0400

    0930

---
+++ 0930.R
@@ -0,0 +1,16 @@
+## Runing the image
+# docker run --rm -it -v ${PWD}:/data datasciencetoolbox/dsatcl2e
+###### Ch2
+
+# where is the ~/.zshrc configuration file for zsh?
+# shift+Enter gives > in a new line of CL input
+# how the gramma works between lines?
+
+###### Ch3
+
+
+
+
+
+
+
