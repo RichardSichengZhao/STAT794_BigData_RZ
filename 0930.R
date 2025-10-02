dfd07a3e855624be7eeac99ad11c98c6241fe01d
Richard Zhao <richardsichengzhao@gmail.com>
2025-10-02 16:50:20 -0400

    SQL

--- 0930.R
+++ 0930.R
@@ -1,7 +1,7 @@
 ## Runing the image
-# docker run --rm -it -v ${PWD}:/data datasciencetoolbox/dsatcl2e
+# docker run --rm -it -v ${PWD}:/home/dst/data datasciencetoolbox/dsatcl2e
 ###### Ch2
-
+# 
 # where is the ~/.zshrc configuration file for zsh?
 # shift+Enter gives > in a new line of CL input
 # how the gramma works between lines?
